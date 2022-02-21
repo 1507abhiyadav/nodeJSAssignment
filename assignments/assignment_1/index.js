d23ce30373dcec372b8a51e540440a08540afbd1
1507abhiyadav <1507abhiyadav@gmail.com>
2022-02-21 12:29:05 +0530

    my assignment-1 sub

--- assignments/assignment_1/index.js
+++ assignments/assignment_1/index.js
@@ -1,13 +1,26 @@
 function getNameFromCommandLine() {
     // Write you code here, name should be taken as args in process.argv
+    // console.log(process)
+    // console.log(process.argv)
+    return process.argv[5];
 }
 
 function getNameFromEnv() {
     // Write your code here
-}
+    //   Process.env.name_node="asdfghjk";
+    //   console.log(process.env)
+    return process.env.name;
+
+
+ }
 
 function getNameFromReadLine() {
     // Write your code here
+    const readline = require("readline");
+    const rl = readline.createInterface({
+        input:process.stdin,
+        output:process.stdout
+    })
 }
 
 module.exports = {
