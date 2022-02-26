583c3c71a8fb2c16c2a64e00fa2296ac765f97b4
1507abhiyadav <1507abhiyadav@gmail.com>
2022-02-26 12:13:47 +0530

    my assin\gnment -3 submission

--- assignments/assignment_1/index.js
+++ assignments/assignment_1/index.js
@@ -16,12 +16,21 @@ function getNameFromEnv() {
 
 function getNameFromReadLine() {
     // Write your code here
-    const readline = require("readline");
-    const rl = readline.createInterface({
-        input:process.stdin,
-        output:process.stdout
-    })
-}
+//     
+      const readline = require("readline");
+      const rl = readline.createInterface({
+          input:process.stdin,
+          output:process.stdout
+      })
+    //   rl.question('What is your age? ', (age) => {
+    //     console.log('Your age is: ' + age);
+    // });
+  
+
+ }
+// getNameFromCommandLine()
+// getNameFromEnv()
+getNameFromReadLine()
 
 module.exports = {
     getNameFromCommandLine,
